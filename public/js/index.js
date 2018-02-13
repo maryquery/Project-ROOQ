3ab3568abe6b595f5d5c9880d9b00bcae2612d4b
Maria Ambroselli <maria.ambroselli@invia.de>
2018-02-13 15:16:41 +0100

    add Navigation + Animations

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,20 @@
 console.log('ready');
 
-//$(document).ready(function() {
+$(document).ready(function() {
 
-//});
+
+});
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("wrapper").style.marginLeft = "250px";
+    document.getElementById("wrapper").style.backgroundColor = "rgb(198, 199, 199)";
+    document.getElementById("iconNav").style.display = "none";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "2.2em";
+    document.getElementById("wrapper").style.marginLeft= "0";
+    document.getElementById("wrapper").style.backgroundColor = "#fbede6";
+    document.getElementById("iconNav").style.display = "block";
+}
