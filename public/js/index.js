aa2787b8d7c76af226dda6ba5da3884583912e20
Maria Ambroselli <maria.ambroselli@invia.de>
2018-09-14 16:06:03 +0200

    Update all stuff new - new html + js + scss

--- public/js/index.js
+++ public/js/index.js
@@ -1,29 +1,40 @@
 console.log('ready');
 
-$(document).ready(function() {
+$(document).ready(function(){
+  // Add scrollspy to <body>
+  //$('#content').scrollspy({target: "nav", offset: 0});
 
-  //$('.fa-sort-down').click(function() {
-    //$(this).toggleClass('fa-sort-up fa-sort-down');
-    //$('#navContent').toggleClass('open close');
-  //});
+  // Add smooth scrolling on all links inside the navbar
+  $("nav a").on('click', function(event) {
 
-  $( "header" ).click(function() {
-    $( "#navContent" ).toggle("slow");
-    $(".arrow").toggleClass('fa-sort-up fa-sort-down');
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('#content').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    }  // End if
+  });
+
+  // active nav
+  $(function() {
+     $("nav a").click(function() {
+        // remove classes from all
+        $("nav a").removeClass("active");
+        // add class to the one we clicked
+        $(this).addClass("active");
+     });
   });
 
 });
-
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("wrapper").style.marginLeft = "250px";
-    document.getElementById("wrapper").style.backgroundColor = "rgb(198, 199, 199)";
-    document.getElementById("iconNav").style.display = "none";
-}
-
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "2.2em";
-    document.getElementById("wrapper").style.marginLeft= "0";
-    document.getElementById("wrapper").style.backgroundColor = "#fbede6";
-    document.getElementById("iconNav").style.display = "block";
-}
---
+++ public/js/musicplayer.js
@@ -0,0 +1,111 @@
+/// Audio Player - date@sep18 -
+var audio;
+
+//Hide Pause button
+$('.pauseBtn').hide();
+
+//Initilize Audio
+initAudio( $('#playlist li:first-child'));
+
+//Initilizer Function
+function initAudio(element){
+  var song = element.attr('song');
+  var title = element.text();
+  var cover = element.attr('cover');
+  var artist = element.attr('artist');
+
+  //Create  Audio Object
+  audio = new Audio('src/audio/' + song);
+
+  //wenn der song nicht gestartet ist
+  if(!audio.currentTime){
+    $('#duration').html('0.00');
+  }
+
+  //Artist in title
+  $('#musicPlayer .title').text(title);
+  $('#musicPlayer .artist').text(artist);
+
+  //Insert Cover Image
+  $('img.cover').attr('src','img/'+ cover);
+
+  $('#playlist li').removeClass('active');
+  element.addClass('active');
+}
+
+//Play Button
+$('.playBtn').click(function(){
+    audio.play();
+    $('.playBtn').hide();
+    $('.pauseBtn').show();
+    $('#duration').fadeIn(400);
+    showDuration();
+});
+
+//Pause Button
+$('.pauseBtn').click(function(){
+    audio.pause();
+    $('.pauseBtn').hide();
+    $('.playBtn').show();
+});
+
+//Stop Button
+$('.stopBtn').click(function(){
+    audio.pause();
+    audio.currentTime = 0;
+    $('.pauseBtn').hide();
+    $('.playBtn').show();
+    $('#duration').fadeOut(400);
+});
+
+//Next Button
+$('.nextBtn').click(function(){
+    audio.pause();
+    var next = $('#playlist li.active').next();
+
+    if(next.lenght  == 0){
+      next = $('#playlist li:first-child');
+    }
+    initAudio(next);
+    audio.play();
+    showDuration();
+});
+
+//Prev Button
+$('.prevBtn').click(function(){
+    audio.pause();
+    var prev = $('#playlist li.active').prev();
+
+    if(prev.lenght  == 0){
+      prev = $('#playlist li:last-child');
+    }
+    initAudio(prev);
+    audio.play();
+    showDuration();
+});
+
+//Volume control
+$('#volume').change(function(){
+  audio.volume = parseFloat(this.value / 10);
+});
+
+//Time duration
+function showDuration(){
+  $(audio).bind('timeupdate', function(){
+    //Get Hours & Minutes
+    var s = parseInt(audio.currentTime % 60);
+    var m = parseInt((audio.currentTime) / 60) % 60;
+    //Add 0 if less than 10
+    if(s<10){
+      s = '0'+ s;
+    }
+    $('#duration').html(m + '.' + s);
+    var value = 0;
+    if(audio.currentTime > 0){
+      value = Math.floor((100 / audio.duration) * audio.currentTime);
+    }
+    $('#progress').css('width', value + '%');
+  })
+
+
+}
