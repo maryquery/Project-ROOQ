934fdb68a24fc8d929e1e1e4773039e014064df1
Maria Ambroselli <maria.ambroselli@invia.de>
2018-09-28 15:47:16 +0200

    stuff

--- public/js/index.js
+++ public/js/index.js
@@ -1,40 +1,31 @@
-console.log('ready');
-
 $(document).ready(function(){
-  // Add scrollspy to <body>
-  //$('#content').scrollspy({target: "nav", offset: 0});
 
-  // Add smooth scrolling on all links inside the navbar
-  $("nav a").on('click', function(event) {
+  $(".main").onepage_scroll({
+     sectionContainer: "section",     // sectionContainer accepts any kind of selector in case you don't want to use section
+     easing: "ease",                  // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in",
+                                      // "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
+     animationTime: 1000,             // AnimationTime let you define how long each section takes to animate
+     pagination: true,                // You can either show or hide the pagination. Toggle true for show, false for hide.
+     updateURL: false,                // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
+     beforeMove: function(index) {
 
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
+     },  // This option accepts a callback function. The function will be called before the page moves.
+     afterMove: function(index) {
+       var page = index;
+       console.log(index);
 
-      // Store hash
-      var hash = this.hash;
+       $('.active .headline').toggleClass('animated bounceInDown');
 
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('#content').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    }  // End if
+     },   // This option accepts a callback function. The function will be called after the page moves.
+     loop: true,                     // You can have the page loop back to the top/bottom when the user navigates at up/down on the first/last page.
+     keyboard: true,                  // You can activate the keyboard controls
+     responsiveFallback: false,        // You can fallback to normal page scroll by defining the width of the browser in which
+                                      // you want the responsive fallback to be triggered. For example, set this to 600 and whenever
+                                      // the browser's width is less than 600, the fallback will kick in.
+     direction: "vertical"            // You can now define the direction of the One Page Scroll animation. Options available are "vertical" and "horizontal". The default value is "vertical".
   });
 
-  // active nav
-  $(function() {
-     $("nav a").click(function() {
-        // remove classes from all
-        $("nav a").removeClass("active");
-        // add class to the one we clicked
-        $(this).addClass("active");
-     });
-  });
+
+
 
 });
